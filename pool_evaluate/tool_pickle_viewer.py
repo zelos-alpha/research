1f6b18585a24347d40b9586a13ab8cea65da170a
32ethers <somereasons@outlook.com>
2023-07-12 11:10:33 +0800

    remove immature project

--- pool_evaluate/tool_pickle_viewer.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-if __name__ == "__main__":
-    file_path = "/home/sun/AA-labs-FE/14_uni_pool_evaluate/data/position_liquidity.pkl"
-    with open(file_path, "rb") as f:
-        content = pickle.load(f)
-
-    pass
